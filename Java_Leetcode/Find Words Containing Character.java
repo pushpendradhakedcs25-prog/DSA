eb415b9a50c64be7fc3879bb16f45600e18e8962
Nam_Do_San <dhakedpushpendra7@gmail.com>
2026-09-21 00:08:13 +0530

    add

---
+++ Java_Leetcode/Find Words Containing Character.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> list = new ArrayList<>();
+        int n= words.length;
+        for(int i=0; i<n; i++){
+           
+                if(words[i].indexOf(x)!=-1){
+                    list.add(i);
+                   
+                }
+            
+        }
+        return list;
+    }
+}
